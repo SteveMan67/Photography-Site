b21a925d6dbc4f9a42b34c5fc8526b52ca3de002
SteveMan67 <timslawncare.ok@gmail.com>
2026-04-18 15:52:42 -0500

    added custom post type for images

--- wp-content/themes/photography/functions.php
+++ wp-content/themes/photography/functions.php
@@ -1,19 +1,53 @@
 <?php
+
 /**
  * Photography Child Theme
  */
 
-function photography_site_google_fonts_preconnect() {
+function photography_site_google_fonts_preconnect()
+{
   echo '<link rel="preconnect" href="https://fonts.googleapis.com">';
   echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
 }
 
 
-function photography_site_enqueue_fonts() {
+function photography_site_enqueue_fonts()
+{
   $font_url = "https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap";
 
-  wp_enqueue_style( 'photgraphy-site-google-fonts', $font_url, array(), null);
+  wp_enqueue_style('photgraphy-site-google-fonts', $font_url, array(), null);
 }
 
-add_action( 'wp_head', 'photography_site_google_fonts_preconnect');
-add_action( 'wp_enqueue_scripts', 'photography_site_enqueue_fonts');
+add_action('wp_head', 'photography_site_google_fonts_preconnect');
+add_action('wp_enqueue_scripts', 'photography_site_enqueue_fonts');
+
+add_action('init', 'register_slide_cpt_and_tax');
+
+function register_slide_cpt_and_tax()
+{
+  $tax_args = array(
+    'labels' => array(
+      'name' => 'Slide Tags',
+      'singular_name' => 'Slide Tag',
+    ),
+    'hierarchical' => false,
+    'public' => true,
+    'show_admin_column' => true
+  );
+  register_taxonomy('slide_tag', array('slide'), $tax_args);
+
+  $ctp_args = array(
+    'labels' => array(
+      'name' => 'Slides',
+      'singular_name' => 'Slide',
+      'add_new_item' => 'Add New Slide',
+    ),
+    'public' => false,
+    'show_ui' => true,
+    'menu_position' => 20,
+    'menu_icon' => 'dashicons-images-alt2',
+    'supports' => array('title', 'thumbnail'),
+    'has_archive' => false,
+  );
+  register_post_type('slide', $ctp_args);
+}
